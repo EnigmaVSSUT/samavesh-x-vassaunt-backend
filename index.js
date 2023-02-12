fe5b9aed1b4c242c1d38dccc37f3657958df1b6d
preritagrawal06 <agrawalprerit780@gmail.com>
2023-02-12 21:12:47 +0530

    mongo uri added

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const cors = require('cors')
 const userRouter = require('./routes/userRoute')
+
 
 app.use(cors())
 app.use(express.json())
@@ -10,7 +12,7 @@ app.use(express.json())
 app.use(userRouter)
 
 mongoose.set("strictQuery", false)
-mongoose.connect("mongodb+srv://Prerit:w6XsTqg1NSCKjk8A@cluster0.wh6pqjs.mongodb.net/events?retryWrites=true&w=majority")
+mongoose.connect(process.env.MONGODB_URI)
     .then(()=>{
         app.listen(8000, ()=>{
             console.log("connected to db")
