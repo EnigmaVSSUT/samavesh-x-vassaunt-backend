e9369be971f4c857e9b3de5b26be7225a9f5e9ff
preritagrawal06 <agrawalprerit780@gmail.com>
2023-02-13 23:31:57 +0530

    jwt added for event participation

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,9 @@
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const createToken = (_id)=>{
+    return jwt.sign({ _id }, process.env.SECRET, { expiresIn:'1d' })
+}
 
 exports.postSignUp = async(req, res, next)=>{
     const {username, email, password, isVssutian, regdNo} = req.body
@@ -6,30 +11,48 @@ exports.postSignUp = async(req, res, next)=>{
     try{
 
         const user = await User.signup(username, email, password, isVssutian, regdNo)
-        res.json(user)
+        const token = createToken(user._id)
+        res.json({email, token})
 
     }catch(err){
-        res.json({'msg': err.message});
+        res.json({msg: err.message});
     }
 }
 
 exports.postLogin = async(req, res, next)=>{
     const {email, password} = req.body
 
-    try{
+    try{ 
         const user = await User.login(email, password)
-        res.json(user)
+        const token = createToken(user._id)
+
+        res.json({email, token})
     }catch(err){
-        res.json({'msg': err.message})
+        res.json({msg: err.message})
     }
 }
 
 exports.postAddEvent = async(req, res, next)=>{
-    const {userId, event} = req.body
+    // console.log(req.headers)
+    const {authorization} = req.headers
+    const {event} = req.body
 
-    User.findById(userId).then(async(user) => {
+    if(!authorization){
+        res.json({msg: "Login to participate"})
+        return
+    }
+
+    // console.log(authorization);
+    const token = authorization.split(" ")[1]
+    // console.log(token);
+
+    const {_id} = jwt.verify(token, process.env.SECRET)
+
+    // console.log(_id)
+
+    User.findById(_id).then(async(user) => {
         // console.log(user)
         const addEvent = await user.addEvents(event)
         res.json(addEvent)
-    }).catch(err => {res.json({'msg': err.message})})
+    }).catch(err => {res.json({msg: err.message})})
 }
