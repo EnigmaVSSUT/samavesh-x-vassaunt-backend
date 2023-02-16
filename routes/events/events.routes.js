1f78fc85ed7495b6094d51895be9b754ac61a400
Pallav <pallavinvizag@gmail.com>
2023-02-16 15:06:12 +0530

    withdraw added

--- routes/events/events.routes.js
+++ routes/events/events.routes.js
@@ -2,8 +2,9 @@ const router = require('express').Router();
 const tokenValidator = require('../../middleware/tokenVerify')
 const getEvents = require("../../controllers/events/getEvents");
 const participate = require("../../controllers/events/participate")
-
+const withdraw = require("../../controllers/events/withdraw")
 router.get("/getEvents", getEvents);
 router.post("/participate", tokenValidator, participate);
+router.post("/withdraw", tokenValidator, withdraw);
 
 module.exports = router
