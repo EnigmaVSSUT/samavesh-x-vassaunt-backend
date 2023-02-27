885b108b2c0e7a7a70cbd5b3c866eef8fc1033eb
Pallav <pallavinvizag@gmail.com>
2023-02-28 00:12:13 +0530

    getusers

--- routes/events/events.routes.js
+++ routes/events/events.routes.js
@@ -7,10 +7,12 @@ const getParticipations = require("../../controllers/events/getParticipations");
 const isPaid = require("../../controllers/Auth/isPaid");
 const getEventsById = require("../../controllers/events/getEventsById")
 const getEventsByClub = require("../../controllers/events/getEventsByClub")
+const getExpo = require("../../controllers/events/getExpo")
 router.get("/getEvents", getEvents);
 router.put("/participate", tokenValidator, isPaid, participate);
 router.put("/withdraw", tokenValidator, withdraw);
 router.get("/getParticipations", tokenValidator, getParticipations);
 router.get("/getEventsByClub/:club", getEventsByClub);
 router.get("/getEventById/:eventId", getEventsById);
+router.get("/getExpo", getExpo);
 module.exports = router;
