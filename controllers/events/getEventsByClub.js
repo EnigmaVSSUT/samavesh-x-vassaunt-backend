2f2c4cf5040766bd07db8999b22120d283141a3e
Pallav <pallavinvizag@gmail.com>
2023-02-27 17:28:53 +0530

    exorts

--- controllers/events/getEventsByClub.js
+++ controllers/events/getEventsByClub.js
@@ -3,14 +3,18 @@ const Event = require("../../models/events");
 const Club = require("../../models/club")
 const getEventsByClub = async (req, res, next) => {
 
-    Event.find({ organiser: req.params.club }).then((events) => {
+    Event.find({ organiser: req.params.club }).then((events, err) => {
+        if (!err)
 
-        Club.find({ name: req.params.club }).then((club) => {
+            Club.find({ name: req.params.club }).then((club) => {
 
 
 
-            res.json({ message: "events fetched", events, club, poc: [{ one: club.facultyAdvisor, two: club.facultyAdvisorPhoto, three: club.facultyAdvisorDepartment }, { one: club.poc2, two: club.poc2pic, three: club.poc2ph }, { one: club.poc1, two: club.poc1pic, three: club.poc1ph }] });
-        })
+                res.json({ message: "events fetched", events, club, poc: [{ one: club.facultyAdvisor, two: club.facultyAdvisorPhoto, three: club.facultyAdvisorDepartment }, { one: club.poc2, two: club.poc2pic, three: club.poc2ph }, { one: club.poc1, two: club.poc1pic, three: club.poc1ph }] });
+            }).catch((err) => {
+                res.json({ message: "Error occured!" })
+            });
+        else res.json({ message: "Error occured!" })
     })
 
 
