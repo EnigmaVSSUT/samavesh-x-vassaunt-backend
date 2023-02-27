7e584220c651ab6eac28b3fac2c736644233df63
Pallav <pallavinvizag@gmail.com>
2023-02-27 16:55:06 +0530

    exorts

--- controllers/events/getEventsByClub.js
+++ controllers/events/getEventsByClub.js
@@ -1,12 +1,14 @@
 const User = require("../../models/user");
 const Event = require("../../models/events");
-
+const Club = require("../../models/club")
 const getEventsByClub = async (req, res, next) => {
-    Event.find({ organiser: req.params.club }).then(async (events) => {
-        res.json({ message: "Events are ready!", events });
-
-    }).catch((err) => {
-        res.json({ message: "Events fetching error" });
+    let club;
+    let events;
+    await Event.find({ organiser: req.params.club }).then((eve) => {
+        events = eve
     })
+    await Club.find({ name: req.params.club }).then(clu => club = clu)
+    res.json({ message: "events fetched", events, club });
+
 }
 module.exports = getEventsByClub;
