cb6b3ac31c1651bef13a1f76917cccd9ccb2e22b
Pallav <pallavinvizag@gmail.com>
2023-03-02 03:13:22 +0530

    OTP template changed!

--- controllers/Auth/otp.js
+++ controllers/Auth/otp.js
@@ -22,56 +22,108 @@ const sendOTPVerification = async (req, res, next) => {
       subject: "Verify your Email",
       html: `
       <html>
-        <body>
-          <div
-          class="otp"
+  <body
+    style="
+      margin: 0;
+      padding: 0;
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: center;
+      overflow: hidden;
+      width: 100vw;
+    "
+  >
+    <div
+      class="otp"
+      style="
+        background-color: #ed1d24;
+        height: 90%;
+
+        width: 100%;
+        border-radius: 10px;
+        color: #ffffff;
+        font-size: 1.5rem;
+        padding: 1.5rem 2.5rem;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: space-between;
+        font-family: 'helvetica';
+      "
+    >
+      <div
+        style="
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+          justify-content: space-between;
+          width: 100%;
+        "
+      >
+        <img
+          src="https://raw.githubusercontent.com/EnigmaVSSUT/samavesh-x-vassaunt/main/public/vssutlogo.png"
+          alt=""
+          style="height: 100px"
+        />
+
+        <img
+          src="https://raw.githubusercontent.com/EnigmaVSSUT/samavesh-x-vassaunt/kishor/pictures/svlogo.png"
+          alt=""
+          height="100px"
+        />
+      </div>
+      <div>
+        <h2>YOUR OTP FOR REGISTRATION IS</h2>
+        <div
           style="
-            background-color: #ff0000;
-            height: fit-content;
-            width: fit-content;
-            border-radius: 10px;
-            color: #ffffff;
-            font-size: 20px;
-            padding: 20px 50px;
+            color: black;
+            background-color: white;
+            padding: 0.3rem;
+            border-radius: 0.3rem;
             text-align: center;
-            font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
           "
+        >
+          <h2
+            style="
+              background-color: #ffffff;
+              padding: 5px;
+              border-radius: 5px;
+              text-align: center;
+            "
           >
-            <div style="display: flex; justify-content: space-between">
-              <img
-                src="https://raw.githubusercontent.com/EnigmaVSSUT/samavesh-x-vassaunt/main/public/vssutlogo.png"
-                alt=""
-                style="height: 60px"
-              />
-              <img
-                src="https://raw.githubusercontent.com/EnigmaVSSUT/samavesh-x-vassaunt/kishor/pictures/svlogo.png"
-                alt=""
-                style="height: 60px"
-              />
-            </div>
-            <h3>YOUR OTP FOR REGISTRATION IS</h3>
-            <h1 style="color: black">
-              <span
-                style="background-color: #ffffff; padding: 5px; border-radius: 5px"
-                >${otp}</span
-              >
-            </h1>
-          
-            <div style="display: flex; align-items: center; justify-content: space-between; font-size: 12px;">
-              <span>${date.getHours() < 10 ? '0' + date.getHours() : date.getHours()}:${date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()}:${date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds()}</span>
-              <span style="display: flex; align-items: center;">
-                <a href="https://enigmavssut.com/" target="_blank">
-                  <img
-                    src="https://raw.githubusercontent.com/EnigmaVSSUT/samavesh-x-vassaunt/kishor/pictures/Enigmalogo.png"
-                    alt=""
-                    style="height: 30px; position: relateive"
-                  />
-                </a>
-              </span>
-            </div>
-          </div>
-        </body>
-      </html>
+            ${otp}
+          </h2>
+        </div>
+      </div>
+
+      <div
+        style="
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+          font-size: 12px;
+          width: 100%;
+        "
+      >
+        <h3>${date.getHours() < 10 ? '0' + date.getHours() :
+          date.getHours()}:${date.getMinutes() < 10 ? '0' + date.getMinutes() :
+            date.getMinutes()}:${date.getSeconds() < 10 ? '0' + date.getSeconds()
+              : date.getSeconds()}</h3>
+        <span style="display: flex; align-items: center">
+          <a href="https://enigmavssut.com/" target="_blank">
+            <img
+              src="https://raw.githubusercontent.com/EnigmaVSSUT/samavesh-x-vassaunt/kishor/pictures/Enigmalogo.png"
+              alt=""
+              style="height: 70px"
+            />
+          </a>
+        </span>
+      </div>
+    </div>
+  </body>
+</html>
+
       `,
     };
 
