113a695c363c4191139c2621d21cac0430262bef
preritagrawal06 <agrawalprerit780@gmail.com>
2023-03-02 22:21:25 +0530

    bug fixed

--- controllers/events/participate.js
+++ controllers/events/participate.js
@@ -9,27 +9,7 @@ module.exports = async (req, res) => {
     User.findByIdAndUpdate(userId, { $addToSet: { events: await req.body.eventId } }, { new: true }).then(async (participated) => {
 
         Event.findByIdAndUpdate(await req.body.eventId, { $addToSet: { participants: userId } }, { new: true }).then(async (event) => {
-
-            const transporter = nodemailer.createTransport({
-                host: "smtp.gmail.com",
-                auth: {
-                    user: process.env.EMAIL,
-                    pass: process.env.PASSWORD,
-                },
-            });
-
-            const mailOption = {
-                from: process.env.EMAIL,
-                to: participated.email,
-                subject: `Successfully registered for the event ${event.eventName}`,
-                html: `<p>Hurray!! You have successfully registered for the event ${event.eventName}.<br>See you on <b>day ${event.day}</b> at <b>${event.startTime} Hrs</b> at <b>${event.venue}</b>.</p>`
-            }
-
-            await transporter
-                .sendMail(mailOption)
-                .then(() => {
-                    res.json({ Message: `Hurray! you are in for ${event.eventName} see you soon!`, participated, success: true })
-                })
+            res.json({ Message: `Hurray! you are in for ${event.eventName} see you soon!`, participated, success: true })
         })
     }).catch((err) => {
         console.log(err)
